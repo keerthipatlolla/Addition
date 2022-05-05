e3864857c00a76dad667f4c9fa4161bb6c34476d
keerthi <keerthi.pulmamidi@gmail.com>
2022-05-05 16:54:15 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Main main = new Main();
+
+        System.out.println("Result: " +main.Addition(10,20));
+
+        System.out.println("Subtraction: " + multipication(15,3));
+
+    }
+    int Addition(int a, int b){
+        return a+b;
+    }
+
+   static int multipication(int x,int y){
+        return x-y;
+   }
+}
